c15fdbd734e05114f135d7553fd1c7a562227e4a
Sovichea Tep <sovichea.tep@gmail.com>
2019-06-02 11:57:35 +0700

    Update license

--- DSM501.cpp
+++ DSM501.cpp
@@ -1,7 +1,7 @@
 /**
 The MIT License (MIT)
 
-Copyright (c) 2015 Sovichea Tep
+Copyright (c) 2019 Sovichea Tep
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
